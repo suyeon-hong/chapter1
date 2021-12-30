54124e91aa12650f4c520c6cc8fb62ea386524ee
suyeonhong <bellahong95@gmail.com>
2021-12-30 15:59:56 +0900

    오류개선

--- checkList/js/main.js
+++ checkList/js/main.js
@@ -2,7 +2,36 @@ const listBox = document.querySelector("ul");
 const userInput = document.querySelector("#userInput");
 const btnAdd = document.querySelector(".btnAdd");
 
-btnAdd.addEventListener("click", ()=>{
+// btnAdd버튼 클릭시 createList 함수 실행
+btnAdd.addEventListener("click", createList);
+
+// listBox내부에서 li클릭시 이벤트발생
+listBox.addEventListener("click", e=>{
+    const target = e.target.closest("li");
+
+    if(target == null) return;
+
+    const checkbox = target.querySelector(".checkbox");
+    const list = target.querySelector(".list");
+    const btnDelete = target.querySelector(".btnDelete");
+
+    // checkbox클릭시 checkbox와 list에 클래스 on 추가
+    if(e.target === checkbox){
+        checkbox.classList.toggle("on");
+        list.classList.toggle("on");
+    }
+
+    // btnDelete버튼 클릭시 해당 li삭제
+    if(e.target === btnDelete) target.remove();
+});
+
+// 엔터키 누를시 createList함수 실행
+window.addEventListener("keydown", e=>{
+    if (e.key == "Enter") createList();
+})
+
+// input값 있을경우에만 list생성하는 함수
+function createList(){
     const value = userInput.value;
     userInput.value = "";
 
@@ -15,27 +44,4 @@ btnAdd.addEventListener("click", ()=>{
         <img src="img/icon_bin.png" class="btnDelete"></img>
     `;
     listBox.append(li);
-});
-
-listBox.addEventListener("click", e=>{
-    const target = e.target.closest("li");
-
-    if(target == null) return;
-
-    const checkbox = target.querySelector(".checkbox");
-    const list = target.querySelector(".list");
-    const btnDelete = target.querySelector(".btnDelete");
-    checkbox.addEventListener("click", ()=>{
-        const isOn = checkbox.classList.contains("on");
-        if(isOn){
-            checkbox.classList.remove("on")
-            list.classList.remove("on")
-        }else{
-            checkbox.classList.add("on")
-            list.classList.add("on")
-        }
-    });
-    btnDelete.addEventListener("click", ()=>{
-        target.remove();
-    });
-});
+}

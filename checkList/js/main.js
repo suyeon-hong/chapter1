67d40af46935c202073fa4972cd7ac3a62332579
suyeonhong <bellahong95@gmail.com>
2021-12-30 18:51:39 +0900

    이벤트함수 개선

--- checkList/js/main.js
+++ checkList/js/main.js
@@ -1,50 +1,48 @@
-const listBox = document.querySelector(".items");
-const userInput = document.querySelector(".footer__input");
-const btnAdd = document.querySelector(".footer__button");
+const items = document.querySelector(".items");
+const input = document.querySelector(".footer__input");
+const addBtn = document.querySelector(".footer__button");
+let i = 0;
 
-// btnAdd버튼 클릭시 createList 함수 실행
-btnAdd.addEventListener("click", createList);
+addBtn.addEventListener("click", createList);
 
-// listBox내부에서 li클릭시 이벤트발생
-listBox.addEventListener("click", e=>{
-    const target = e.target.closest(".item");
+items.addEventListener("click", e=>{
+    const id = e.target.dataset.id;
+    if(id == null) return;
 
-    if(target == null) return;
+    const item = items.querySelector(`.item[data-id="${id}"]`);
+    const checkbox = item.querySelector(".item__check");
+    const list = item.querySelector(".item__name");
+    const btnDelete = item.querySelector(".item__delete i");
 
-    const checkbox = target.querySelector(".item__check");
-    const list = target.querySelector(".item__name");
-    const btnDelete = target.querySelector(".item__delete i");
-
-    // checkbox클릭시 checkbox와 list에 클래스 on 추가
     if(e.target === checkbox){
         checkbox.classList.toggle("on");
         list.classList.toggle("on");
     }
 
-    // btnDelete버튼 클릭시 해당 li삭제
-    if(e.target === btnDelete) target.remove();
+    if(e.target === btnDelete) item.remove();
 });
 
-// 엔터키 누를시 createList함수 실행
 window.addEventListener("keydown", e=>{
     if (e.key == "Enter") createList();
 })
 
-// input값 있을경우에만 list생성하는 함수
 function createList(){
-    const value = userInput.value;
-    userInput.value = "";
+    const value = input.value;
+    input.value = "";
+    input.focus();
 
     if(!value) return;
     
     const li = document.createElement("li");
-    li.classList.add("item")
+    li.setAttribute("class", "item");
+    li.setAttribute("data-id", i);
     li.innerHTML = `
-        <span class="item__check"></span>
-        <p class="item__name">${value}</p>
-        <button class="item__delete">
-            <i class="fas fa-trash-alt"></i>
-        </buttonsrc=>
+        <span class="item__check" data-id="${i}"></span>
+        <p class="item__name" data-id="${i}">${value}</p>
+        <button class="item__delete"><i class="fas fa-trash-alt" data-id="${i}"></i></button>
     `;
-    listBox.append(li);
+    i++;
+
+    items.append(li);
+    li.scrollIntoView({block: "end"});
 }

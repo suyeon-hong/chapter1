0570b1132752b4ad7355ddb147ff64685493fe14
suyeonhong <bellahong95@gmail.com>
2022-01-02 00:05:24 +0900

    minigame displayImg함수 추가

--- minigame/js/main.js
+++ minigame/js/main.js
@@ -2,11 +2,17 @@ const btnStart = document.querySelector(".btnStart");
 const timer = document.querySelector(".timer");
 const sec = timer.querySelector(".sec");
 const ms = timer.querySelector(".ms");
+const num = document.querySelector(".number");
+const stage = document.querySelector(".stage");
+let remainCarrot = 10;
 
-btnStart.addEventListener("click", ()=>{
+btnStart.addEventListener("click", startGame);
+
+function startGame(){
     setTimer();
-
-})
+    countCarrot();
+    displayImg();
+}
 
 function setTimer(){
     let seconds = 5;
@@ -32,4 +38,33 @@ function setTimer(){
         millisec--;
         ms.innerText = millisec;
     }, 10)
+}
+
+function countCarrot(){
+    num.innerText = remainCarrot;
+}
+
+function displayImg(){
+    for(let i = remainCarrot; i>0; i--){
+        let randomNum = Math.random()*1200;
+        let randomNum2 = Math.random()*300;
+        randomNum = parseInt(randomNum);
+        randomNum2 = parseInt(randomNum2);
+        console.log(randomNum);
+
+        stage.innerHTML += `
+            <img src="img/carrot.png" alt="carrot" style="transform: translate(${randomNum}px, ${randomNum2}px)">
+        `
+    }
+    for(let i = remainCarrot - 3; i>0; i--){
+        let randomNum = Math.random()*1200;
+        let randomNum2 = Math.random()*300;
+        randomNum = parseInt(randomNum);
+        randomNum2 = parseInt(randomNum2);
+        console.log(randomNum);
+
+        stage.innerHTML += `
+            <img src="img/bug.png" alt="bug" style="transform: translate(${randomNum}px, ${randomNum2}px)">
+        `
+    }
 }
